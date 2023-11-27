e07eb106e832df20fcb86817a4874b4d4d091f85
Alex Brik <115663358+AlexB528@users.noreply.github.com>
2023-11-27 15:41:34 -0500

    add function to add characters

--- controllers/characterController.js
+++ controllers/characterController.js
@@ -1,9 +1,22 @@
 const Character = require("../models/character");
-
+const characterCoordinates = require("../temp/characterCoordinates")
 const asyncHandler = require("express-async-handler");
 
 // Display list of all Authors.
 exports.send_Characters = asyncHandler(async (req, res, next) => {
     const characters = await Character.find({});
     res.json(characters);
+});
+
+exports.add_Characters = asyncHandler(async (req, res, next) => {
+
+        // Create a Book object with escaped and trimmed data.
+        const character = new Character({
+          name: req.body.name,
+          coordinatesList: req.body.characterCoordinates,
+          difficulty: req.body.difficulty,
+        });
+
+        await character.save();
+        return res.json({message: 'successful save'});
 });
